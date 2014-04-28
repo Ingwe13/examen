a118580b9766c189e813567021259cd8cec080fe
Javier Mariscal <>
2014-04-28 19:12:31 +0200

    ver 1.2

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+/**
+ *
+ * @author usuario
+ */
+public class Sudoku {
+    private final int TAMMATRIZ = 9;
+    
+  
+    public Sudoku() {
+        String[][] sol = new String[TAMMATRIZ][TAMMATRIZ];
+          }
+         
+//    char solucionSudoku[][] = { {'.',8,'.','.','.','.',2,'.','.',},
+//                        {'.','.','.','.',8,4,'.',9,'.'},
+//                        {'.','.',6,3,2,'.','.',1,'.'},
+//                        {'.',9,7,'.','.','.','.',8,'.'},
+//                        {8,'.','.',9,'.',3,'.','.',2},
+//                        {'.',1,'.','.','.','.',9,5,'.'},
+//                        {'.',7,'.','.',4,5,8,'.','.'},
+//                        {'.',3,'.',7,1,'.','.','.','.'},
+//                        {'.','.',8,'.','.','.','.',4,'.'}
+//                      };
+
+    
+
+
+}
